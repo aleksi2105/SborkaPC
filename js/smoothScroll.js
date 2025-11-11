0550946bb8643a973d003cb060247c54ff7dedb7
aleksi2105 <aleksi2105@yandex.ru>
2025-11-11 03:00:00 +0300

    Скролл реализован через библиотеку seamless

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -5,13 +5,16 @@ links.forEach((link) => {
   link.addEventListener('click', (event) => {
     event.preventDefault()
     
+    
     const section = document.querySelector(link.getAttribute('href'))
     
     if (section) {
-      section.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth'
-      })
+     
+      seamless.scrollIntoView(section, {
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+    });
     }
   })
 })
